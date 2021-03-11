efc11adff5461ebcbd4cc88a5b4c500c784e38f4
ivangayton <ivangayton@gmail.com>
2021-03-11 23:07:57 +0900

    indentation error

--- attachments.py
+++ attachments.py
@@ -23,44 +23,48 @@ def threaded_download():
 
 def all_attachments_from_form(url, aut, project, form, outdir):
     """Downloads all available attachments from a given form"""
-     submissions = fetch.submissions(url, aut, project, form)
+    submissions = fetch.submissions(url, aut, project, form)
 
-     for submission in submissions.json():
-         sub_id = submission['instanceId']
-         print(sub_id)
-         attachments = fetch.attachment_list(url, aut, project, form, sub_id)
-         for attachment in attachments.json():
-             fn = attachment['name']
-             attresp = fetch.attachment(url, aut, project, form,
+    for submission in submissions.json():
+        sub_id = submission['instanceId']
+        print(sub_id)
+        attachments = fetch.attachment_list(url, aut, project, form, sub_id)
+        for attachment in attachments.json():
+            fn = attachment['name']
+            attresp = fetch.attachment(url, aut, project, form,
                                                sub_id, fn)
-             outfilepath = os.path.join(outdir, fn)
-             with open(outfilepath, 'wb') as outfile:
-                 outfile.write(attresp.content)
+            outfilepath = os.path.join(outdir, fn)
+            with open(outfilepath, 'wb') as outfile:
+                outfile.write(attresp.content)
 
 def specified_attachments_from_form(url, aut, project, form, outdir, infile):
-    """Downloads attchments specified in an input text file from a form."""
+    """Downloads attachments specified in a text file."""
+    wantedfiles = []
     try:
         inputfile = open(infile)
-        wantedfiles = 
+        
             
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('-url', '--base_url',
                    help = 'Server URL')
     p.add_argument('-u', '--user',
-                   help = 'ODK Central username (usually email)')
+                   help = 'ODK Central username (usually email).')
     p.add_argument('-pw', '--password',
-                   help = 'ODK Central password')
+                   help = 'ODK Central password.')
     p.add_argument('-p', '--project',
                    help = 'the project in question')
     p.add_argument('-f', '--form',
-                   help = 'Unique name of the relevant form')
+                   help = 'Unique name of the relevant form.')
     p.add_argument('-i', '--instance',
                    help = 'Submission instance ID')
     p.add_argument('-od', '--output_directory',
-                   help = 'Directory to write output files')
+                   help = 'Directory to write output files.')
     p.add_argument('-t', '--threads',
                    help = 'Maximum number of download threads', default = 10)
+    p.add_argument('-i', '--input_file',
+                   help = 'Text file listing desired attachments. '\
+                   'Should contain only filenames separated by line breaks.')
 
     args = p.parse_args()
 
